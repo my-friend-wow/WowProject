54edd2ca42913753ae8b3749a99f5fefb740dbd5
heewoneha <jhjmo0719h@naver.com>
2023-10-07 22:40:08 +0900

    fix: add cronjob time and run cronjob background

--- back_end/entrypoint.sh
+++ back_end/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cron cron/reset_step_by_count.py
+echo "0 15 * * * root /usr/bin/python3 /app/server/cron/reset_step_by_count.py" > /etc/cron.d/cronjob
+chmod 644 /etc/cron.d/cronjob
+crontab /etc/cron.d/cronjob
+
+cron -f &
+
 gunicorn -b 0.0.0.0:5000 run:app --error-logfile=/var/log/gunicorn/error.log --access-logfile=/var/log/gunicorn/access.log --max-requests 1000 --max-requests-jitter 50
