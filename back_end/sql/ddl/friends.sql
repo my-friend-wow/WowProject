cfaaf19cd8d17973c5bfb8e3b8e100d5155eb338
heewoneha <jhjmo0719h@naver.com>
2023-11-20 22:37:15 +0900

    fix: add FK and arrange SQL query

--- back_end/sql/ddl/friends.sql
+++ back_end/sql/ddl/friends.sql
@@ -1,12 +1,12 @@
 CREATE TABLE industry.friends -- UNNEST
 (
     user_id        NVARCHAR(20) NOT NULL, -- 유저 아이디 (FK)
-    friend_doll_id NVARCHAR(255) NOT NULL -- 친구 doll_id
+    friend_doll_id NVARCHAR(255) NOT NULL, -- 친구 doll_id
 
-    CONSTRAINT PK_friends PRIMARY KEY (user_id, friend_doll_id)
+    CONSTRAINT PK_friends
+        PRIMARY KEY (user_id, friend_doll_id),
+    
+    CONSTRAINT FK_friends
+        FOREIGN KEY (user_id) REFERENCES industry.users(user_id)
+        ON DELETE CASCADE
 );
-
-ALTER TABLE industry.friends
-ADD CONSTRAINT FK_friends
-FOREIGN KEY (user_id) REFERENCES industry.users(user_id)
-ON DELETE CASCADE;
